b2fb08fe9a1a9f4f8665c684ea8c78d00468e71c
Mayur Bagul <mayurbagul107@gmail.com>
2026-05-19 19:22:00 +0530

    Time: 4 ms (100.00%) | Memory: 46 MB (33.79%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || x%10==0 && x!=0){
+            return false;
+        }
+        int revhalf = 0;
+        while(x > revhalf){
+            revhalf=revhalf*10+x%10;
+            x/=10;
+        }
+        return (x==revhalf || x==revhalf / 10);
+    }
+}
