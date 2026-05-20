41df265dadedee612b10c414460b613e51769a5b
Mayur Bagul <mayurbagul107@gmail.com>
2026-05-20 22:01:12 +0530

    Time: 5 ms (45.03%) | Memory: 42.7 MB (5.02%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -8,13 +8,11 @@ class Solution {
             return 0;
         else if(n==1)
             return 1;
-        else{
-            for(int i=2; i<=n; i++){
+        for(int i=2; i<=n; i++){
                 c=a+b;
                 a=b;
                 b=c;
             }
-        }
         return b;
     }
 }
