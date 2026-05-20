79f579d10bcf00e62f628b08b8586c8b2651dff4
Mayur Bagul <mayurbagul107@gmail.com>
2026-05-20 22:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (15.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,15 +1,14 @@
 
 class Solution {
     public int fib(int n) {
-        Scanner sc = new Scanner(System.in);
-        int a=0, b=1, c=0;
+        int a=0, b=1;
         
         if(n==0)
             return 0;
         else if(n==1)
             return 1;
         for(int i=2; i<=n; i++){
-                c=a+b;
+            int c=a+b;
                 a=b;
                 b=c;
             }
