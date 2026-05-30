13b450be753f3f24d4e055e0de2926a32ab02bec
Mayur Bagul <mayurbagul107@gmail.com>
2026-05-30 22:50:52 +0530

    Time: 34 ms (59.82%) | Memory: 59.1 MB (59.05%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,39 +5,31 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
 
-        for(int i=0; i<n-2; i++)
-        {
-            if(i > 0 && nums[i] == nums[i - 1])
-            {
+        for(int i=0; i<n-2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             int target = -nums[i];
             int left = i + 1;
             int right = n - 1;
 
-            while(left < right)
-            {
+            while(left < right) {
                 int sum = nums[left] + nums[right];
-                if(sum == target)
-                {
+                if(sum == target) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while(left < right && nums[left] == nums[left + 1])
-                    {
+                    while(left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
-                    while(left < right && nums[left] == nums[right - 1])
-                    {
+                    while(left < right && nums[left] == nums[right - 1]) {
                         right--;
                     }
                     left++;
                     right--;
                 }
-                else if(sum < target)
-                {
+                else if(sum < target) {
                     left++;
                 }
-                else
-                {
+                else {
                     right--;
                 }
             }
