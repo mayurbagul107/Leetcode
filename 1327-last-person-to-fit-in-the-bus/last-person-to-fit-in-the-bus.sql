4a965efc8999e095961d38c23b8c6b527ba5b59b
Mayur Bagul <250089652+mayurbagul107@users.noreply.github.com>
2026-07-23 20:21:54 +0530

    Time: 1971 ms (5.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select q1.person_name
+from Queue q1 Inner Join Queue q2
+ON q1.turn >= q2.turn 
+group by q1.turn
+having SUM(q2.weight) <= 1000
+order by SUM(q2.weight) DESC
+limit 1
