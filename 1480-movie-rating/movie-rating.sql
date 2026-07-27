3d43ef64bb35f318dab4d994b9529ad59a7b8889
Mayur Bagul <250089652+mayurbagul107@users.noreply.github.com>
2026-07-27 22:49:37 +0530

    Time: 1556 ms (22.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+(select name as results
+from Users
+inner join MovieRating USING (user_id)
+group by user_id
+order by count(rating) DESC, name
+limit 1
+)
+
+UNION ALL
+
+(select title as results
+from Movies
+inner join MovieRating USING(movie_id)
+where month(created_at) = '02' AND year(created_at) = '2020'
+group by title 
+order by AVG(rating) DESC, title
+limit 1
+)
